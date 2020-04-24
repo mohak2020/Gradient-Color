c5cea2cf18c122a559b9175e189641442c748a32
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2020-04-25 02:32:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+console.log(css);
+console.log(color1);
+console.log(color2);
+console.log(body);
+
+function setColor() {
+
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	// console.log(body.style.background);
+	// console.log("linear-gradient(to right, " 
+	// + color1.value 
+	// + ", " 
+	// + color2.value 
+	// + ")");
+
+	css.textContent = body.style.background + ";";
+
+}
+
+color1.addEventListener("input",setColor);
+
+color2.addEventListener("input", setColor);
